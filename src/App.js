cd8d46cac7f8635e177926d725259168dcd261e1
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-18 17:39:38 +0400

    banner created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import NavBar from "./Components/NavBar/NavBar";
 
 //TODO:3 i want remove the default padding and margin. so i created one App.css file and import into App.js
 import './App.css'
+import Banner from "./Components/Banner/Banner";
 
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner/>
       
       
     </div>
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+//TODO:5 Create style for banner.js in Banner.css
+import './Banner.css'
+
+//TODO:4 create a banner page 
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie Name</h1>
+            <div className='banner_buttons'>
+                <button className='button'>Play</button>
+                <button className='button'>My list</button>
+            </div>
+            <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the </h1>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
