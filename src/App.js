4d376881aa0119779bf127fd7695b0827e85229b
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-18 16:19:59 +0400

    navbar margin and padding issue solved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
 
+//TODO:3 i want remove the default padding and margin. so i created one App.css file and import into App.js
+import './App.css'
+
 
 function App() {
   return (
