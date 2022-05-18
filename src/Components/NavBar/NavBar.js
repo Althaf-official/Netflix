fd244ab4af0fb9458ab21f5bb0562ae94c8ee769
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-18 16:11:04 +0400

    successfully created navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 //Import navbar.css
 import "./NavBar.css"
-//TODO:1 import css file.  create logo and avatar 
+//TODO:1 import css file.  create logo and avatar
+
 
 
 function NavBar() {
   return (
       //put one logo and avatar which is taked from   https://github.com/Packapeer/React_tutorial_netflix_app/blob/main/assets.md 
       // give className to div & logo and avatar a className
+      //TODO:2 give style to className= navbar and logo. avatar
     <div  className='navbar'>
         <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="" />
         <img className='avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Avatar" />
