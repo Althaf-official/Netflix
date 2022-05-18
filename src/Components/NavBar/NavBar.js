94817aeea022caeb7ad199c7395dbfcde8ce7803
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-18 15:24:58 +0400

    logo and avatar added

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+
+
 function NavBar() {
   return (
+      //put one logo and avatar which is taked from   https://github.com/Packapeer/React_tutorial_netflix_app/blob/main/assets.md 
     <div >
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="" />
+        <img src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Avatar" />
       
     </div>
   )
