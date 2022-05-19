4026447cce74fde7a67f4dffbfb4c860f1617e48
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-19 20:22:17 +0400

    setState dynamic image - create a variable for image databse

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -29,13 +29,14 @@ function Banner() {
            //for 1st data printing 
            console.log(response.data.results[0]);
            // in the console will show the object. if you click on the object it will show the details of the movie so i will copy the movie details.which part i need  copy the property then call it with the {movie.title or movie.overview etc...}
-           setMovie(response.data.results[0])
+           setMovie(response.data.results[16])
+           //TODO:14 CHANGE BANNER IMAGE
         })
     },[])
   return (
     <div className='banner'>
         <div className='content'>
-            <h1 className='title'>{movie &&movie.title}</h1>
+            <h1 className='title'>{movie && movie.title}</h1>
             <div className='banner_buttons'>
                 <button className='button'>Play</button>
                 <button className='button'>My list</button>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,3 +4,9 @@ export const baseUrl ='https://api.themoviedb.org/3'
 
 //API_KEY is a key which is i created in themoviedb.org website so i will take that api key
 export const API_KEY="da1bcc8de71c9f7bd69205cd3dfa5ed9";
+
+//TODO:15 store the image-database-url to a variable
+//this url is for the movie database image storing platform
+export const imageUrl ='https://image.tmdb.org/t/p/original'
+
+
