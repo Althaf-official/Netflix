3e48038aa76f7aa4e42fe04288dcf44d66149949
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-20 06:38:34 +0400

    row post   succesfully work props. change movie gener as netflix origials and action

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     <div className="App">
       <NavBar/>
       <Banner/>
-      <RowPost/>
-      <RowPost/>
+      <RowPost title='Netflix Originals'/>
+      <RowPost title='Action' isSmall />
       
     </div>
   );
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -5,7 +5,7 @@ import './RowPost.css'
 import {API_KEY,baseUrl, imageUrl} from '../../constants/constants'
 
 //TODO:6 now we are going to add movie poster 
-function RowPost() {
+function RowPost(props) {
     const [movies,setMovies] = useState([])
     useEffect(()=>{
         // IF YOU WANT EDIT IN THE LINK HERE SHOULD BE USE"     `   ${API_KEY}  `   "LIKE THIS
@@ -18,7 +18,7 @@ function RowPost() {
     },[])
   return (
     <div className='row'>
-      <h2>Netflix Originals</h2>
+      <h2>{props.title}</h2>
       <div className='posters'>
           {movies.map((obj)=>
               <img className='poster' src={`${imageUrl+obj.backdrop_path}`} alt="" />
