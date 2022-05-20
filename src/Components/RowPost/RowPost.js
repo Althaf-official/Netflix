39ae127479b3e16c5a8c6d3f5139ba1dfa7e97d5
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-20 05:58:15 +0400

    row post -import API KEY

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,16 +1,18 @@
 //TODO:14 ROW POST MAKE DYNAMIC
-import Reac,{useEffect,useState} from 'react'
+import React,{useEffect,useState} from 'react'
 import axios from '../../axios'
 import './RowPost.css'
-import { useEffect } from 'react'
+import {API_KEY} from '../../constants/constants'
 
 //TODO:6 now we are going to add movie poster 
 function RowPost() {
     useEffect(()=>{
         axios.get('discover/tv?api_key={API_KEY_HERE}&with_networks=213').then(response=>{
             console.log(response)
+        }).catch(err=>{
+            alert('Network Error')
         })
-    })
+    },[])
   return (
     <div className='row'>
       <h2>Title</h2>
