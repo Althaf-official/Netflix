18c192bef7134f89330cafcdfde638836dbbe88c
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-20 06:13:39 +0400

    rowpost- setMovies in useEffect axios

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -11,27 +11,17 @@ function RowPost() {
         // IF YOU WANT EDIT IN THE LINK HERE SHOULD BE USE"     `   ${API_KEY}  `   "LIKE THIS
         axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then(response=>{
             console.log(response)
+            setMovies(response.data.results)
         }).catch(err=>{
-            alert('Network Error')
+           // alert('Network Error')
         })
     },[])
   return (
     <div className='row'>
-      <h2>Title</h2>
+      <h2>Netflix Originals</h2>
       <div className='posters'>
           <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
-          <img className='poster' src="https://pngbackground.com/public/uploads/thumbnail/horror-tree-poster-background-for-picsart-cb-editing-11638661320psxisepqqr.jpg" alt="" />
+         
 
 
       </div>
