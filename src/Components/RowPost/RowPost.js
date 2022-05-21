abc5c8e141dd9d05c0df05426dfa4826df102933
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-21 20:29:01 +0400

    show video - use react-player is not working properly

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,43 +1,47 @@
 //TODO:14 ROW POST MAKE DYNAMIC
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from '../../axios'
 import './RowPost.css'
-import {imageUrl,API_KEY} from '../../constants/constants'
+import { imageUrl, API_KEY } from '../../constants/constants'
 import ReactPlayer from 'react-player'
 
 //TODO:6 now we are going to add movie poster 
 function RowPost(props) {
-    const [movies,setMovies] = useState([])
-    const [urlid,setUrlId] = useState('')
-    useEffect(()=>{
+    const [movies, setMovies] = useState([])
+    const [urlid, setUrlId] = useState('')
+    useEffect(() => {
         // IF YOU WANT EDIT IN THE LINK HERE SHOULD BE USE"     `   ${API_KEY}  `   "LIKE THIS
-        axios.get(props.url).then(response=>{
+        axios.get(props.url).then(response => {
             console.log(response)
             setMovies(response.data.results)
-        }).catch(err=>{
-           // alert('Network Error')
+        }).catch(err => {
+            // alert('Network Error')
         })
-    },[])
-    const handleMovie = (id) =>{
+    }, [])
+    const handleMovie = (id) => {
         console.log(id)
-        axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-            console.log(response.data)
+        axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response => {
+            if (response.data.results.length !== 0) {
+                setUrlId(response.data.results[0])
+            } else {
+                console.log('Array empty')
+            }
         })
 
     }
 
 
-  return (
-    <div className='row'>
-      <h2>{props.title}</h2>
-      <div className='posters'>
-          {movies.map((obj)=>
-              <img onClick={()=>handleMovie(obj.id)}  className={props.isSmall ? `smallPoster`:'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
-          )}
-      </div>  
-      <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U'  width="100%" />
-    </div>
-  )
+    return (
+        <div className='row'>
+            <h2>{props.title}</h2>
+            <div className='posters'>
+                {movies.map((obj) =>
+                    <img onClick={() => handleMovie(obj.id)} className={props.isSmall ? `smallPoster` : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt="poster" />
+                )}
+            </div>
+            {urlid && <ReactPlayer videoid={urlid.key} width="100%" />}
+        </div>
+    )
 }
 
 export default RowPost
