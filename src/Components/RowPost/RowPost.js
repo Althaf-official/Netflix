67d6ac15616a47706cee1fdadfbe242b20913758
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-21 15:25:52 +0400

    show video - react-youtube npm pakage was not working so i installed React-player

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -3,7 +3,7 @@ import React,{useEffect,useState} from 'react'
 import axios from '../../axios'
 import './RowPost.css'
 import {imageUrl} from '../../constants/constants'
-import Youtube from 'react-youtube'
+import ReactPlayer from 'react-player'
 
 //TODO:6 now we are going to add movie poster 
 function RowPost(props) {
@@ -19,10 +19,11 @@ function RowPost(props) {
     },[])
     const opts = {
         height: '390',
-        width: '640',
+        width: '100%',
         playerVars: {
           // https://developers.google.com/youtube/player_parameters
           autoplay: 1,
+
         },
       };
   return (
@@ -32,8 +33,8 @@ function RowPost(props) {
           {movies.map((obj)=>
               <img className={props.isSmall ? `smallPoster`:'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
           )}
-      </div>
-      <Youtube opts={opts} videoId="2g811Eo7K8U"/>
+      </div>  
+      <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
     </div>
   )
 }
