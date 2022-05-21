395ced912bd7a45fe3e4bd812233c4bd5e64b529
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-21 21:10:51 +0400

    show video - succesfully showing video when i click the poster

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -8,7 +8,7 @@ import ReactPlayer from 'react-player'
 //TODO:6 now we are going to add movie poster 
 function RowPost(props) {
     const [movies, setMovies] = useState([])
-    const [urlid, setUrlId] = useState('')
+    const [urlId, setUrlId] = useState('')
     useEffect(() => {
         // IF YOU WANT EDIT IN THE LINK HERE SHOULD BE USE"     `   ${API_KEY}  `   "LIKE THIS
         axios.get(props.url).then(response => {
@@ -21,7 +21,8 @@ function RowPost(props) {
     const handleMovie = (id) => {
         console.log(id)
         axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response => {
-            if (response.data.results.length !== 0) {
+            if (response.data.results.length != 0) {
+                console.log(response.data.results[0])
                 setUrlId(response.data.results[0])
             } else {
                 console.log('Array empty')
@@ -39,7 +40,7 @@ function RowPost(props) {
                     <img onClick={() => handleMovie(obj.id)} className={props.isSmall ? `smallPoster` : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt="poster" />
                 )}
             </div>
-            {urlid && <ReactPlayer videoid={urlid.key} width="100%" />}
+           {urlId && <ReactPlayer url={`https://www.youtube.com/watch?v=${urlId.key}`} width="100%" />}
         </div>
     )
 }
