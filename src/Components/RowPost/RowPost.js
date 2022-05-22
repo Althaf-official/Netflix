896b00e44c83ff805b55b3bf0c50d72e2a192079
ALTHAF SHARAFUDHEEN <althaf.sharafudheen@outlook.com>
2022-05-22 05:53:14 +0400

    posters - make cursor pointer and hover effect

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -21,7 +21,7 @@ function RowPost(props) {
     const handleMovie = (id) => {
         console.log(id)
         axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response => {
-            if (response.data.results.length != 0) {
+            if (response.data.results.length !== 0) {
                 console.log(response.data.results[0])
                 setUrlId(response.data.results[0])
             } else {
